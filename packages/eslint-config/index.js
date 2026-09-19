4c609a9ebafc825649cf1caaeeac7c6f7804c20c
Michael Labus <10035371+mlabus93@users.noreply.github.com>
2026-09-18 17:54:40 -0700

    chore: tighten TypeScript project configuration

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -1,11 +1,12 @@
 const eslint = require("@eslint/js");
+const { defineConfig } = require("eslint/config");
 const eslintConfigPrettier = require("eslint-config-prettier");
 const globals = require("globals");
 // TypeScript 7 has no compiler API yet, so this package aliases `typescript`
 // to `@typescript/typescript6` for typescript-eslint. Editors and builds use TS 7.
 const tseslint = require("typescript-eslint");
 
-module.exports = tseslint.config(
+module.exports = defineConfig(
   {
     ignores: ["dist/**"],
   },
@@ -13,7 +14,7 @@ module.exports = tseslint.config(
   ...tseslint.configs.recommended,
   eslintConfigPrettier,
   {
-    files: ["**/*.ts"],
+    files: ["**/*.{ts,tsx}"],
     languageOptions: {
       globals: globals.browser,
     },
